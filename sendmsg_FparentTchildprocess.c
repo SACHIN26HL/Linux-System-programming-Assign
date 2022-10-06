9fc4be2b4bfe71ad3de171672c46bd96ba9a8264
SACHIN26HL <sachingowda739@gmail.com>
2022-10-06 11:09:07 +0530

    create parent and child process

---
+++ sendmsg_FparentTchildprocess.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+
+int main()
+{
+	int id;
+
+	char msg[100];
+
+	id = fork();
+
+	if(id == 0)
+	{
+		printf("I'm child process\n");
+		printf("Child process id : %d\n", getpid());
+
+
+
+	}
+	else if(id > 0)
+	{
+		printf("I'm parent process\n");
+		printf("parent process id : %d\n" , getppid());
+		
+
+
+	}
+	else
+	{
+		printf("Error\n");
+	}
+
+
+
+	return 0;
+}
